32334b0f91953dced4e84260f5348a4bf0cfc350
bahgaat <bahgat.reda90@gmail.com>
2021-09-02 16:16:03 +0200

    Add all

--- src/hashtable.c
+++ src/hashtable.c
@@ -6,16 +6,16 @@
  * This creates a new hash table of the specified size and with
  * the given hash function and comparison function.
  */
-HashTable *createHashTable(int size, unsigned int (*hashFunction)(void *),
-                           int (*equalFunction)(void *, void *)) {
+HashTable *createHashTable(int size, unsigned int (*hashFunction)(void *),  // hasFunction is apointer to a function that returns usigned int.
+                           int (*equalFunction)(void *, void *)) {  // equalFunction is apointer to a function that returns int
   int i = 0;
-  HashTable *newTable = malloc(sizeof(HashTable));
+  HashTable* newTable = (HashTable*) malloc(sizeof(HashTable));
   if (NULL == newTable) {
     fprintf(stderr, "malloc failed \n");
     exit(1);
   }
   newTable->size = size;
-  newTable->data = malloc(sizeof(HashBucket *) * size);
+  newTable->data = malloc(sizeof(HashBucket) * size);
   if (NULL == newTable->data) {
     fprintf(stderr, "malloc failed \n");
     exit(1);
@@ -35,20 +35,65 @@ HashTable *createHashTable(int size, unsigned int (*hashFunction)(void *),
  * call insertData(someHashTable, (void *) string, (void *) string).
  */
 void insertData(HashTable *table, void *key, void *data) {
-  // -- TODO --
-  // HINT:
   // 1. Find the right hash bucket location with table->hashFunction.
+  unsigned int HashBucketLocation = table->hashFunction(key); // this will return to me an index from the array of hashBuckets
   // 2. Allocate a new hash bucket struct.
-  // 3. Append to the linked list or create it if it does not yet exist. 
+  HashBucket* newBucket = (HashBucket*) malloc(sizeof(HashBucket));
+  if (NULL == newBucket) {
+    fprintf(stderr, "malloc failed \n");
+    exit(1);
+  }
+  newBucket->key = key;
+  newBucket->data = data;
+  newBucket->next = NULL;
+  // 3. Append to the linked list or create it if it does not yet exist.
+  HashBucket* buckets = table->data[HashBucketLocation];
+  if (buckets == NULL) {
+    table->data[HashBucketLocation] = newBucket;
+  } else {
+    while (buckets->next != NULL) {
+      buckets = buckets -> next;
+    }
+    buckets->next = newBucket;
+  }
 }
 
 /*
  * This returns the corresponding data for a given key.
- * It returns NULL if the key is not found. 
+ * It returns NULL if the key is not found.
  */
 void *findData(HashTable *table, void *key) {
-  // -- TODO --
-  // HINT:
   // 1. Find the right hash bucket with table->hashFunction.
+  unsigned int HashBucketLocation = table->hashFunction(key);
+  if (table->data == NULL) {
+    return NULL;
+  }
+  HashBucket* bucket = table->data[HashBucketLocation];
   // 2. Walk the linked list and check for equality with table->equalFunction.
+  if (bucket == NULL) {
+    return NULL;
+  }
+  while (bucket != NULL) {
+    if (table->equalFunction(key, bucket->key) != 0) {
+      return bucket->data;
+    }
+    bucket = bucket->next;
+  }
+  return NULL;
+}
+
+void freeTable(HashTable *table) {
+  HashBucket** hashBuckets = table->data;
+  for (int i = 0; i < table->size; i++) {
+    HashBucket* bucket = hashBuckets[i];
+    while (bucket != NULL) {
+      free(bucket->key);
+      free(bucket->data);
+      HashBucket* oldBucket = bucket;
+      bucket = bucket->next;
+      free(oldBucket);
+    }
+  }
+  free(hashBuckets);
+  free(table);
 }
--- src/philphix.c
+++ src/philphix.c
@@ -3,6 +3,8 @@
  */
 #include "hashtable.h"
 
+#include <math.h>
+
 /*
  * Include the header file.
  */
@@ -28,6 +30,8 @@
  */
 #include <string.h>
 
+#include <errno.h>
+
 /*
  * This hash table stores the dictionary.
  */
@@ -35,7 +39,7 @@ HashTable *dictionary;
 
 /*
  * The MAIN routine.  You can safely print debugging information
- * to standard error (stderr) as shown and it will be ignored in 
+ * to standard error (stderr) as shown and it will be ignored in
  * the grading process.
  */
 int main(int argc, char **argv) {
@@ -51,11 +55,9 @@ int main(int argc, char **argv) {
 
   fprintf(stderr, "Loading dictionary %s\n", argv[1]);
   readDictionary(argv[1]);
-  fprintf(stderr, "Dictionary loaded\n");
-
   fprintf(stderr, "Processing stdin\n");
   processInput();
-
+  freeTable(dictionary);
   /*
    * The MAIN function in C should always return 0 as a way of telling
    * whatever program invoked this that everything went OK.
@@ -68,22 +70,27 @@ int main(int argc, char **argv) {
  * to a char * (null terminated string)
  */
 unsigned int stringHash(void *s) {
-  // -- TODO --
-  fprintf(stderr, "need to implement stringHash\n");
-
-  /* To suppress compiler warning until you implement this function, */
-  return 0;
+  //fprintf(stderr, "need to implement stringHash\n");
+  unsigned int hash = 0;
+  char *string = (char*) s;
+  for (int i = 0; i < strlen(string); i++) {
+    hash = 31 * hash + string[i];
+  }
+  unsigned int index = hash % 0x61C; //(Here has to be the size of hashtable)
+  return index;
 }
 
 /*
- * This should return a nonzero value if the two strings are identical 
+ * This should return a nonzero value if the two strings are identical
  * (case sensitive comparison) and 0 otherwise.
  */
 int stringEquals(void *s1, void *s2) {
-  // -- TODO --
-  fprintf(stderr, "You need to implement stringEquals");
-  /* To suppress compiler warning until you implement this function */
-  return 0;
+  int isDifferent = strcmp((char *) s1, (char *) s2);
+  if (isDifferent == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 /*
@@ -98,21 +105,69 @@ int stringEquals(void *s1, void *s2) {
  * NOT exist, you should print some message to standard error and call exit(61)
  * to cleanly exit the program.
  */
+ void resizeIfNeeded(int counter, char **originalKeyString, char **originalValueString, int findKey,
+                    int *sizeOFString, char **string);
+char* findTheReplacableWord(char* string);
+void checkMalloc(char* string);
+
 void readDictionary(char *dictName) {
-  // -- TODO --
-  fprintf(stderr, "You need to implement readDictionary\n");
+  //Step1 : open the file.
+  FILE *fp;
+  fp = fopen(dictName, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Specify a dictionary\n");
+    exit(61);
+  }
+  //Step2 : read the words one at a time and insert into dictionary.
+  char ch;
+  int sizeOFString = 61;
+  char *originalKeyString = (char*) malloc(sizeOFString * sizeof(char)); /*+1 for '\0' character */
+  char *originalValueString = (char*) malloc(sizeOFString * sizeof(char)); /*+1 for '\0' character */
+  checkMalloc(originalKeyString);
+  checkMalloc(originalValueString);
+  char *string = originalKeyString;
+  int findKey = 0;
+  unsigned int counter = 0;
+  while ((ch = fgetc(fp))!= EOF) {
+    resizeIfNeeded(counter, &originalKeyString, &originalValueString, findKey, &sizeOFString, &string);
+    if (isspace(ch) && findKey == 0) { // the word has ended
+      *string = '\0'; //it has to be leading zero
+      findKey = 1;
+      string = originalValueString;
+      counter = 0;
+      sizeOFString = 61;
+    } else if (ch == '\n') {
+      *string = '\0';
+      insertData(dictionary, (void *) originalKeyString, (void *) originalValueString);
+      originalKeyString = (char*) malloc(61*sizeof(char));
+      originalValueString = (char*) malloc(61*sizeof(char));
+      checkMalloc(originalKeyString);
+      checkMalloc(originalValueString);
+      string = originalKeyString;
+      sizeOFString = 61;
+      findKey = 0;
+      counter = 0;
+    } else if (!isspace(ch)) {
+      *string = ch;
+      string ++;
+      counter ++;
+    }
+  }
+  *string = '\0';
+  insertData(dictionary, (void *) originalKeyString, (void *) originalValueString);
+  fclose(fp);
 }
 
 /*
- * This should process standard input (stdin) and perform replacements as 
- * described by the replacement set then print either the original text or 
- * the replacement to standard output (stdout) as specified in the spec (e.g., 
- * if a replacement set of `taest test\n` was used and the string "this is 
- * a taest of  this-proGram" was given to stdin, the output to stdout should be 
+ * This should process standard input (stdin) and perform replacements as
+ * described by the replacement set then print either the original text or
+ * the replacement to standard output (stdout) as specified in the spec (e.g.,
+ * if a replacement set of `taest test\n` was used and the string "this is
+ * a taest of  this-proGram" was given to stdin, the output to stdout should be
  * "this is a test of  this-proGram").  All words should be checked
  * against the replacement set as they are input, then with all but the first
  * letter converted to lowercase, and finally with all letters converted
- * to lowercase.  Only if all 3 cases are not in the replacement set should 
+ * to lowercase.  Only if all 3 cases are not in the replacement set should
  * it report the original word.
  *
  * Since we care about preserving whitespace and pass through all non alphabet
@@ -122,10 +177,84 @@ void readDictionary(char *dictName) {
  *
  * Do note that even under the initial assumption that no word is longer than 60
  * characters, you may still encounter strings of non-alphabetic characters (e.g.,
- * numbers and punctuation) which are longer than 60 characters. Again, for the 
+ * numbers and punctuation) which are longer than 60 characters. Again, for the
  * final bit of your grade, you cannot assume words have a bounded length.
  */
 void processInput() {
-  // -- TODO --
-  fprintf(stderr, "You need to implement processInput\n");
+  char ch;
+  int sizeOFString = 61;
+  char *string = (char*) malloc(sizeOFString * sizeof(char));
+  checkMalloc(string);
+  int counter = 0;
+  int findString = 0;
+  char *helperString = string;
+  while ((ch = getchar())!= EOF) {
+    resizeIfNeeded(counter, &string, &string, 0, &sizeOFString, &helperString);
+    if (isalnum(ch)) {
+      *helperString = ch;
+      findString = 0;
+      counter ++;
+      helperString ++;
+    } else if (findString == 0) {
+      *helperString = '\0';
+      findString = 1;
+      char *replacableWord = findTheReplacableWord(string);
+      if (replacableWord != NULL) {
+        printf("%s", replacableWord);
+      } else {
+        printf("%s", string);
+      }
+      putchar(ch);
+      free(string);
+      counter = 0;
+      sizeOFString = 61;
+      string = (char*) malloc(sizeOFString * sizeof(char));
+      checkMalloc(string);
+      helperString = string;
+    } else {
+      putchar(ch);
+    }
+  }
+  free(string);
+}
+
+void resizeIfNeeded(int counter, char **originalKeyString, char **originalValueString, int findKey,
+                   int *sizeOFString, char **string) {
+  if (counter >= *sizeOFString - 1) {
+    *sizeOFString = *sizeOFString * 2;
+    if (findKey == 1) {
+      *originalValueString = (char *) realloc(*originalValueString, *sizeOFString);
+      *string = *originalValueString + counter;
+    } else {
+      *originalKeyString = (char *) realloc(*originalKeyString, *sizeOFString);
+      *string = *originalKeyString + counter;
+    }
+  }
+}
+
+char* findTheReplacableWord(char* string) {
+  char cpyString[strlen(string) + 1];
+  strcpy(cpyString, string);
+  char* replacableWord;// = (char*) findData(dictionary, string);
+  for (int i = 0; i < 3; i++) {
+    if (i == 1) {
+      for (int j = 1; j < strlen(cpyString); j++) {
+        cpyString[j] = tolower(cpyString[j]);
+      }
+    } else if (i == 2) {
+        *cpyString = tolower(*cpyString);
+    }
+    replacableWord = (char*) findData(dictionary, cpyString);
+    if (replacableWord != NULL) {
+      return replacableWord;
+    }
+  }
+  return replacableWord;
+}
+
+void checkMalloc(char* string) {
+  if (NULL == string) {
+    fprintf(stderr, "malloc failed \n");
+    exit(1);
+  }
 }
